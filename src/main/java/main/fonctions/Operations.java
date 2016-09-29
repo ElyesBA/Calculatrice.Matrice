3a813f34c855dcee3425af90ddd15debeca83cae
Elyes <elyes.benabdelkader@esprit.tn>
2016-09-29 21:48:57 +0100

    Code Update
    Signed-off-by: Elyes <elyes.benabdelkader@esprit.tn>

--- src/main/java/main/fonctions/Operations.java
+++ src/main/java/main/fonctions/Operations.java
@@ -4,54 +4,36 @@ import main.structures.Matrice;
 
 public class Operations {
 
-	public Matrice addition(Matrice a, Matrice b) {
-		return null;
-	}
+	public static Matrice addition(Matrice a, Matrice b) {
+		int lignes = a.getLignes();
+		int colonnes = a.getColonnes();
+		double[][] resultat = new double[lignes][colonnes];
 
-	public Matrice soustraction(Matrice a, Matrice b) {
-		return null;
-	}
-
-	public Matrice multiplication(double d, Matrice a) {
-		return null;
-	}
-
-	public Matrice multiplication(Matrice a, Matrice b) {
-		return null;
-	}
-
-	private double determinant(double[][] a, int n) {
-		double det = 0;
-		int sign = 1, p = 0, q = 0;
-
-		if (n == 1) {
-			det = a[0][0];
-		} else {
-			double b[][] = new double[n - 1][n - 1];
-			for (int x = 0; x < n; x++) {
-				p = 0;
-				q = 0;
-				for (int i = 1; i < n; i++) {
-					for (int j = 0; j < n; j++) {
-						if (j != x) {
-							b[p][q++] = a[i][j];
-							if (q % (n - 1) == 0) {
-								p++;
-								q = 0;
-							}
-						}
-					}
+		if (lignes == b.getLignes() && colonnes == b.getColonnes()) {
+			for (int i = 0; i < lignes; i++) {
+				for (int j = 0; j < colonnes; j++) {
+					resultat[i][j] = a.getMatrice()[i][j] + b.getMatrice()[i][j];
 				}
-				det = det + a[0][x] * determinant(b, n - 1) * sign;
-				sign = -sign;
+			}
+
+			return new Matrice(resultat);
+		} else {
+			return null;
+		}
+	}
+
+	public static Matrice multiplication(double d, Matrice a) {
+		int lignes = a.getLignes();
+		int colonnes = a.getColonnes();
+		double[][] resultat = new double[lignes][colonnes];
+
+		for (int i = 0; i < lignes; i++) {
+			for (int j = 0; j < colonnes; j++) {
+				resultat[i][j] = d * a.getMatrice()[i][j];
 			}
 		}
-		return det;
+
+		return new Matrice(resultat);
 	}
 
-	public double determinant(Matrice m, int n) {
-		if (m.getLignes() != m.getLignes())
-			return 0;
-		return determinant(m.getMatrice(), m.getLignes());
-	}
 }
